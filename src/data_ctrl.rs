9f713b831acb284f64fbe72853b65ac42d18813a
coyuki <fu08yu20ko07@icloud.com>
2025-02-03 00:27:36 +0900

    エラーハンドリングの改善とデータ管理機能のリファクタリングを実施

--- src/data_ctrl.rs
+++ src/data_ctrl.rs
@@ -1,32 +1,33 @@
-use crate::PJHelpError;
+use crate::PJError;
 use home::home_dir;
 use std::{fs, path::PathBuf};
 
-fn folder_path() -> Result<PathBuf, PJHelpError> {
+fn folder_path() -> Result<PathBuf, PJError> {
     Ok(home_dir()
-        .ok_or(PJHelpError::FailedGetHome)?
+        .ok_or(PJError::FailedGetHome)?
         .join(".project_helper"))
 }
 
 /// return (tasks.json path, projects.json path)
-/// 各種ファイル、フォルダが存在しない場合に作成. 存在した場合はそのパスを返す.
-pub fn initialize_helper_folder() -> Result<(PathBuf, PathBuf), PJHelpError> {
+/// 各種ファイル、フォルダが存在しない場合には作成.
+pub fn initialize_helper_folder() -> Result<PathBuf, PJError> {
     let set_path = folder_path()?;
-    let tasks_json_path = &set_path.join("tasks.json");
-    let projects_json_path = &set_path.join("projects.json");
+    let data_json_path = &set_path.join("data.json");
+    // let projects_json_path = &set_path.join("projects.json");
+    // let title_index_json_path = &set_path.join("title_index.json");
 
     if !set_path.exists() {
-        fs::create_dir(&set_path).map_err(PJHelpError::IoError)?;
+        fs::create_dir(&set_path).map_err(PJError::IoError)?;
     }
-    if !tasks_json_path.exists() {
-        fs::File::create(tasks_json_path).map_err(PJHelpError::IoError)?;
+    if !data_json_path.exists() {
+        fs::File::create(data_json_path).map_err(PJError::IoError)?;
     }
-    if !projects_json_path.exists() {
-        fs::File::create(projects_json_path).map_err(PJHelpError::IoError)?;
-    }
+    // if !projects_json_path.exists() {
+    //     fs::File::create(projects_json_path).map_err(PJError::IoError)?;
+    // }
+    // if !tasks_json_path.exists() {
+    //     fs::File::create(title_index_json_path).map_err(PJError::IoError)?;
+    // }
 
-    Ok((
-        tasks_json_path.to_path_buf(),
-        projects_json_path.to_path_buf(),
-    ))
+    Ok(data_json_path.to_path_buf())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,15 @@
 mod data_ctrl;
 
-use chrono::{DateTime, Local};
+use chrono::Local;
 use serde::{Deserialize, Serialize};
 use std::{
+    collections::{HashMap, HashSet},
     fmt::{self, Display},
     fs::{self},
     io::{self, BufWriter, Write},
 };
 
-#[derive(Debug)]
-pub enum PJHelpError {
+enum PJError {
     SomeError,
     FailedGetHome,
     IoError(io::Error),
@@ -17,163 +17,189 @@ pub enum PJHelpError {
     FailedConvertJson2T,
 }
 
-impl Display for PJHelpError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            PJHelpError::SomeError => write!(f, ""),
-            PJHelpError::FailedGetHome => write!(f, ""),
-            PJHelpError::IoError(error) => write!(f, "{}", error),
-            PJHelpError::FailedConvertT2Json => write!(f, "failed convert type of Data to json."),
-            PJHelpError::FailedConvertJson2T => write!(f, "failed convert json to type of Data."),
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
+struct TitleIndex {
+    title_index: Vec<String>,
+}
+impl TitleIndex {
+    fn new() -> Self {
+        TitleIndex {
+            title_index: Vec::<String>::new(),
         }
     }
-}
-
-/// A task struct
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Task {
-    title: String,
-    id: u64,
-    notes: Option<String>,
-    status: Status,
-    parent_id: Option<u64>,
-    subtasks: Vec<u64>,
-    created_date: DateTime<Local>,
-    due_date: Option<DateTime<Local>>,
-    project: Project,
-}
-
-/// Task implementation
-impl Task {
-    pub fn new(
-        title: String,
-        notes: Option<String>,
-        status: Status,
-        parent_id: Option<u64>,
-        subtasks: Vec<u64>,
-        due_date: Option<DateTime<Local>>,
-        project: Project,
-    ) -> Self {
-        Task {
-            title,
-            id: 3,
-            notes,
-            status,
-            parent_id,
-            subtasks,
-            created_date: Local::now(),
-            due_date,
-            project,
-        }
+    fn add(&mut self, title: String) {
+        self.title_index.push(title);
+    }
+    fn new_key(&self) -> u64 {
+        self.title_index.len() as u64
+    }
+    fn title_to_key(&self, title: String) -> Option<u64> {
+        Some(self.title_index.iter().position(|t| t == &title)? as u64)
     }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub enum Status {
+struct Relation {
+    parent: Vec<u64>,
+    sub_task: Vec<u64>,
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
+enum Status {
     NotStarted,
     InProgress,
     Done,
 }
-
-impl From<&str> for Status {
-    fn from(s: &str) -> Self {
-        match s {
-            "n" => Status::NotStarted,
-            "i" => Status::InProgress,
-            "d" => Status::Done,
-            _ => Status::NotStarted,
+impl Display for Status {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Status::NotStarted => write!(f, "not started"),
+            Status::InProgress => write!(f, "in progress"),
+            Status::Done => write!(f, "done"),
         }
     }
 }
 
-#[derive(Debug, Deserialize, Serialize)]
-pub struct Tasks {
-    tasks: Vec<Task>,
+#[derive(Serialize, Deserialize, Debug)]
+struct Task {
+    title: String,
+    notes: Option<String>,
+    due_date: Option<chrono::DateTime<Local>>,
+    created_date: chrono::DateTime<Local>,
+    relation: Option<Relation>,
+    status: Status,
+    project: Project,
+    archived: bool,
 }
-impl Tasks {
+impl Task {
     fn new() -> Self {
-        Tasks { tasks: Vec::new() }
-    }
-    pub fn load_or_init() -> Self {
-        // data_ctrl::initialize_helper_folder();
-        Tasks { tasks: Vec::new() }
+        Task {
+            notes: None,
+            due_date: None,
+            created_date: chrono::Local::now(),
+            relation: Some(Relation {
+                parent: Vec::<u64>::new(),
+                sub_task: Vec::<u64>::new(),
+            }),
+            status: Status::Done,
+            project: Project {
+                project: String::new(),
+            },
+            title: String::new(),
+            archived: true,
+        }
     }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Project {
-    name: String,
-    id: u64,
+struct Project {
+    project: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Projects {
-    projects: Vec<Project>,
+    projects: HashSet<String>,
 }
 impl Projects {
     fn new() -> Self {
         Projects {
-            projects: Vec::new(),
+            projects: HashSet::<String>::new(),
         }
     }
-    fn new_id(&self) -> u64 {
+    fn add(&mut self, title: String) -> bool {
+        // self.projects.push(title);
+        self.projects.insert(title)
+    }
+    fn new_key(&self) -> u64 {
         self.projects.len() as u64
     }
-    fn add(&mut self, project: Project) {
-        self.projects.push(project);
+    fn title_to_key(&self, title: String) -> Option<u64> {
+        Some(self.projects.iter().position(|t| t == &title)? as u64)
+    }
+    fn rm(&mut self, target: String) -> bool {
+        self.projects.remove(&target)
     }
 }
 
-#[derive(Debug)]
-pub struct Data {
-    tasks: Tasks,
+#[derive(Serialize, Deserialize, Debug)]
+struct Tasks {
+    tasks: HashMap<u64, Task>,
+    index: TitleIndex,
     projects: Projects,
 }
+impl Tasks {
+    fn new() -> Self {
+        Tasks {
+            tasks: HashMap::new(),
+            index: TitleIndex::new(),
+            projects: Projects::new(),
+        }
+    }
+    fn add(&mut self, task: Task) {
+        let key = self.index.new_key();
+        self.tasks.insert(key, task);
+    }
 
-impl Data {
-    pub fn load() -> Result<Self, PJHelpError> {
+    fn rm(&mut self, target: String) -> Result<(), PJError> {
+        self.tasks
+            .get_mut(&self.index.title_to_key(target).ok_or(PJError::SomeError)?)
+            .ok_or(PJError::SomeError)?
+            .archived = true;
+        Ok(())
+    }
+
+    fn delete(&mut self, target: String) -> Result<(), PJError> {
+        self.tasks
+            .remove(&self.index.title_to_key(target).ok_or(PJError::SomeError)?);
+        Ok(())
+    }
+
+    fn get_info(&self) {
+        todo!()
+    }
+
+    fn get_list(&self) {
+        todo!()
+    }
+
+    fn add_project(&mut self, title: String) {
+        self.projects.add(title);
+    }
+    fn rm_project(&mut self, target: String) -> Result<(), PJError> {
+        if !self.projects.rm(target) {
+            Err(PJError::SomeError)
+        } else {
+            Ok(())
+        }
+    }
+
+    pub fn load() -> Result<Self, PJError> {
         let path_set = data_ctrl::initialize_helper_folder()?;
 
-        let tasks_json = fs::read_to_string(path_set.0).map_err(PJHelpError::IoError)?;
-        let projects_json = fs::read_to_string(path_set.1).map_err(PJHelpError::IoError)?;
+        let data_json = fs::read_to_string(path_set).map_err(PJError::IoError)?;
 
-        Ok(Data {
-            tasks: if tasks_json.is_empty() {
-                Tasks::new()
-            } else {
-                let v: Tasks =
-                    serde_json::from_str(&tasks_json).map_err(|_| PJHelpError::FailedGetHome)?;
-                v
-            },
-            projects: if projects_json.is_empty() {
-                Projects::new()
-            } else {
-                let v: Projects =
-                    serde_json::from_str(&projects_json).map_err(|_| PJHelpError::FailedGetHome)?;
-                v
-            },
+        Ok(if data_json.is_empty() {
+            Tasks::new()
+        } else {
+            let parsed_tasks: Tasks =
+                serde_json::from_str(&fs::read_to_string(&data_json).map_err(PJError::IoError)?)
+                    .map_err(|_| PJError::FailedConvertJson2T)?;
+            parsed_tasks
         })
     }
-    pub fn save(&self) -> Result<(), PJHelpError> {
-        let (tasks_path, projects_path) = data_ctrl::initialize_helper_folder()?;
 
-        let mut tasks_writer =
-            BufWriter::new(fs::File::create(tasks_path).map_err(PJHelpError::IoError)?);
-        let mut pjs_writer =
-            BufWriter::new(fs::File::create(projects_path).map_err(PJHelpError::IoError)?);
+    pub fn save(&self) -> Result<(), PJError> {
+        let path = data_ctrl::initialize_helper_folder()?;
+
+        // 保存のためファイルは上書き。色々面倒なのでforでなく全部ベタ書き
+        let mut tasks_writer = BufWriter::new(fs::File::create(path).map_err(PJError::IoError)?);
 
         // TODO: 空の場合の処理も?
-        let tasks_json =
-            serde_json::to_string(&self.tasks).map_err(|_| PJHelpError::FailedConvertT2Json)?;
-        let projects_json =
-            serde_json::to_string(&self.projects).map_err(|_| PJHelpError::FailedConvertT2Json)?;
+        let tasks_json = serde_json::to_string(&self).map_err(|_| PJError::FailedConvertT2Json)?;
 
-        write!(&mut tasks_writer, "{}", tasks_json).map_err(PJHelpError::IoError)?;
-        write!(&mut pjs_writer, "{}", projects_json).map_err(PJHelpError::IoError)?;
+        write!(&mut tasks_writer, "{}", tasks_json).map_err(PJError::IoError)?;
 
-        tasks_writer.flush().map_err(PJHelpError::IoError)?;
-        pjs_writer.flush().map_err(PJHelpError::IoError)?;
-
+        tasks_writer.flush().map_err(PJError::IoError)?;
         Ok(())
     }
 }
